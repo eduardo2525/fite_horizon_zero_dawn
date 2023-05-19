00b708393e19a45fe3b1b1466f991b935200b723
eduardo <marketingdoeduardo@gmail.com>
2023-05-19 14:07:51 -0300

    coloquei um delei nas navegações com setTimeout

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.querySelector('a[href="#Sobre"]').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('container').scrollIntoView({ behavior: 'smooth' });
+    setTimeout(function() {
+        document.getElementById('container').scrollIntoView({ behavior: 'smooth' });
+    }, 500)
+    
 });
 
 document.querySelector('a[href="#Saiba-mais"]').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('Saiba-mais').scrollIntoView({ behavior: 'smooth'});
+    setTimeout(function() {
+        document.getElementById('Saiba-mais').scrollIntoView({ behavior: 'smooth'});
+    }, 500)
+    
 })
 
 document.querySelector('a[href="#esperiencia"]').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('esperiencia').scrollIntoView({ behavior: 'smooth'});
+    setTimeout(function() {
+        document.getElementById('esperiencia').scrollIntoView({ behavior: 'smooth'});
+    }, 500)
 })
